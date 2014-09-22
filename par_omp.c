1c3f4939a9a5639e17687fee5939cb921704c55f
YE Fan <fye@fandembp.bouyguesbox.fr>
2014-09-22 16:25:36 +0200

    par_omp.c modified. A augmented version of vectorized integral function using intrinsics. When NN=100, 6 to 7 times of acceleration. When NN=1000, 2 to 3 times of acceleration.

--- par_omp.c
+++ par_omp.c
@@ -26,7 +26,7 @@ const unsigned long long M = 1; //Monte Carlo Simulation
 //const real EPSILON = X0*1.0E-2; //threshold value
 const real prob = 0.95;
 
-real NormalIntegral(real b);
+real vNormalIntegral(real b);
 
 int main(int argc, char *argv[])
 {
@@ -77,8 +77,8 @@ int main(int argc, char *argv[])
 	  int j = k*Ncache+i;
 	  real Tj = j*(real)T/N;
 	  upbd = (log(PX[i]/K)+0.5*SIGMA*SIGMA*(T-Tj))/(SIGMA*sqrt(T-Tj));
-	  //errloc -= 1/(sqrt(2*PI))*(PX[i+1]-PX[i])*NormalIntegral(upbd);
-	  err += -1/(sqrt(2*PI))*(PX[i+1]-PX[i])*NormalIntegral(upbd);
+	  //errloc -= 1/(sqrt(2*PI))*(PX[i+1]-PX[i])*vNormalIntegral(upbd);
+	  err += -1/(sqrt(2*PI))*(PX[i+1]-PX[i])*vNormalIntegral(upbd);
 	}
 
 #pragma omp single 
@@ -113,12 +113,12 @@ int main(int argc, char *argv[])
       }
       
       //maybe vary the scheduling strategy?
-#pragma omp for reduction(+:err)
+#pragma omp for reduction(+:err) nowait
       for (int i = 0; i < left; ++i){
 	int j = nCal*Ncache+i;
 	real Tj = j*(real)T/N;
 	upbd = (log(PX[i]/K)+0.5*SIGMA*SIGMA*(T-Tj))/(SIGMA*sqrt(T-Tj));
-	err += -1/sqrt((2*PI))*(PX[i+1]-PX[i])*NormalIntegral(upbd);
+	err += -1/sqrt((2*PI))*(PX[i+1]-PX[i])*vNormalIntegral(upbd);
       }
 
 #pragma omp sections 
@@ -126,14 +126,14 @@ int main(int argc, char *argv[])
 #pragma omp section
 	{
 	  upbd = (log(X0/K) + 0.5*SIGMA*SIGMA*T)/(SIGMA*sqrt(T));
-	  errloc -= X0/(sqrt(2*PI))*NormalIntegral(upbd);
+	  errloc -= X0/(sqrt(2*PI))*vNormalIntegral(upbd);
 #pragma omp atomic
 	  err += errloc;
 	}
 #pragma omp section
 	{
 	  upbd = (log(X0/K) - 0.5*SIGMA*SIGMA*T)/(SIGMA*sqrt(T));
-	  errloc += K/(sqrt(2*PI))*NormalIntegral(upbd);
+	  errloc += K/(sqrt(2*PI))*vNormalIntegral(upbd);
 #pragma omp atomic
 	  err += errloc;
 	}
@@ -162,11 +162,15 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-real NormalIntegral(real b)
+#ifdef __MIC__
+real vNormalIntegral(real b)
 {
-  //if(b < -10.0) return  0.0f;
-  //if(b > 10.0) return 1.0f;
-  const int NN = 10000;
+  __declspec(align(64)) __m512d vec_cf0, vec_cf1, vec_cf2, vec_s, vec_stp, vec_exp; 
+  
+  //const int NN = 1000; //has to be the multiple of 8
+  const int vecsize = 8; 
+  const int nCal = (NN/2-1)/vecsize;
+  //const int left = NN%vecsize;
   real a = 0.0f;
   real s, h, sum = 0.0f;
   h = (b-a)/NN;
@@ -175,7 +179,48 @@ real NormalIntegral(real b)
   // and the last one
   sum += exp(-b*b/2.0);
 
-#pragma simd reduction(+:sum) vectorlengthfor(real) assert
+  vec_cf0 = _mm512_set1_pd(a);
+  vec_cf1 = _mm512_set1_pd(2*h);
+  vec_cf2 = _mm512_set1_pd(-0.5);
+
+  vec_s   = _mm512_set_pd(8,7,6,5,4,3,2,1);//vectorize
+  vec_s   = _mm512_mul_pd(vec_s, vec_cf1);//(16h,14h,..,2h)
+  vec_s   = _mm512_add_pd(vec_cf0, vec_s);//(a+16h,..,a+2h)
+  
+  vec_stp = _mm512_set1_pd(2*h*vecsize-h);
+  vec_cf0 = _mm512_set1_pd(h);
+  
+  for (int i = 0; i < nCal; ++i){
+    vec_exp = _mm512_mul_pd(vec_s, vec_s);
+    vec_exp = _mm512_mul_pd(vec_exp, vec_cf2);
+    vec_cf1 = _mm512_exp_pd(vec_exp);//vec_cf1->sum
+    sum    += 2.0*_mm512_reduce_add_pd(vec_cf1);
+
+    vec_s   = _mm512_add_pd(vec_s, vec_cf0);//s+=h
+    vec_exp = _mm512_mul_pd(vec_s, vec_s);
+    vec_exp = _mm512_mul_pd(vec_exp, vec_cf2);
+    vec_cf1 = _mm512_exp_pd(vec_exp);
+    sum    += 4.0*_mm512_reduce_add_pd(vec_cf1);
+    
+    vec_s   = _mm512_add_pd(vec_s, vec_stp);
+  }
+
+  sum = 0.5*sqrt(2*PI) + h*sum/3.0;
+  return sum;
+}
+#else
+real vNormalIntegral(real b)
+{
+  //const int NN = 100000;
+  real a = 0.0f;
+  real s, h, sum = 0.0f;
+  h = (b-a)/NN;
+  // add in the first few terms 
+  sum += exp(-a*a/2.0) + 4.0*exp(-(a+h)*(a+h)/2.0);
+  // and the last one
+  sum += exp(-b*b/2.0);
+  
+#pragma vector always 
   for (int i = 1; i < NN/2; ++i){
     s = a + 2*i*h;
     sum += 2.0*exp(-s*s/2.0);
@@ -186,3 +231,5 @@ real NormalIntegral(real b)
   sum = 0.5*sqrt(2*PI) + h*sum/3.0;
   return sum;
 }
+#endif
+
