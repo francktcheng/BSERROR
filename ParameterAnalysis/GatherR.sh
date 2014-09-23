f75c63c472240c7efcc557ac7d293fd48845bbf2
Langshi CHEN <chenlangshi@gmail.com>
2014-09-23 15:51:22 +0200

    1) wrting intro part

--- ParameterAnalysis/GatherR.sh
+++ ParameterAnalysis/GatherR.sh
@@ -6,12 +6,12 @@ Chunck=0
 
 for((Chunck=10; Chunck<=100; Chunck+=10)) 
 do
-	echo "Ncache,Chunck,Time,Error,Prob" > bserror-Chunck-$Chunck-Data.csv 
-	for((Ncache=10000; Ncache<=20000; Ncache+=100))
+	echo "Ncache,Chunck,Time,Error,Prob" > bserrorV1-120core-Chunck-$Chunck-Data.csv
+	for((Ncache=100; Ncache<=60000; Ncache+=100))
 	do
-		cat ../bserrorTestResult/par-omp-$Ncache-$Chunck.out | while read oneline
+		cat ../bserrorV1-120core-Result/par-omp-$Ncache-$Chunck.out | while read oneline
 	do 
-		echo $oneline >> bserror-Chunck-$Chunck-Data.csv
+		echo $oneline >> bserrorV1-120core-Chunck-$Chunck-Data.csv
 	done
 	done
 done
